800ac0b2500c830a6c380cf75b903f5933f87aae
yjll1019 <yjll1019@naver.com>
2018-06-10 22:54:01 +0900

    WordInfo 클래스
    WordInfo 클래스

---
+++ Task/20180610/WordInfo.java
@@ -0,0 +1,11 @@
+package algorithmTask2;
+
+public class WordInfo {
+    String word;
+    int count;
+
+    public WordInfo(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+}
